879a292298368e8bb296fc20d92bada117769483
toinan.beroud@gmail.com <toinan.beroud@gmail.com>
2025-04-09 18:01:08 +0200

    simplification de la fonction pour ouvrir differentes colonnes d'un fichier .parquet

--- script/function/open_parquet.R
+++ script/function/open_parquet.R
@@ -5,27 +5,34 @@
 #' 
 #' @param dir le chemin vers le dossier qui contient les chunks .parquet du fichier
 #' @param id le vecteur avec le nom de la colonne identifiant que l'on souhaite conserver
-#' @param col le vecteur avec le nom de la colonne d'interet que l'on souhaite conserver
+#' @param vars le vecteur avec le nom des colonne d'interet que l'on souhaite conserver
 #' 
 #' @return 
 #' La fonction ouvre un objet data.frame
 #' 
 #' @examples
 #' data <- open_parquet(dir = "test/parquet/export/data1/",
-#'                      id = c("IRIS"),
-#'                      col = c("P20_POP"))
+#'                      id = "ID",
+#'                      vars = c("VALUE1"))
 
 library(arrow)
 library(duckdb)
 library(dplyr)
 
-open_parquet <- function(dir, id, col) {
+open_parquet <- function(dir,
+                         id, 
+                         vars) {
   
   # Creation de la connexion a DuckDB pour executer des requetes SQL
   con <- dbConnect(duckdb())
   
   # Recuperation des noms de tous les fichiers .parquet
   parquet <- list.files(dir, pattern = "\\.parquet$", full.names = TRUE)
+  
+  # Verification de la presence de fichiers
+  if (length(parquet) == 0) {
+    stop("Aucun fichier .parquet trouve dans le dossier.")
+  }
   
   # Creation d'une vue SQL pour agreger les fichiers .parquet
   query <- paste0(
@@ -37,31 +44,10 @@ open_parquet <- function(dir, id, col) {
   # Chargement de la vue comme table DuckDB
   tbl_duckdb <- tbl(con, "all_data")
   
-  # Test de toutes les combinaisons possibles entre les differents noms
-  combinaison <- expand.grid(id = id, col = col, stringsAsFactors = FALSE)
-  
-  # Creation du tableau final
-  data <- data.frame()
-  
-  # Boucle pour tester les differentes combinaisons
-  for (i in seq_len(nrow(combinaison))) {
-    comb <- combinaison[i, ]
-    tryCatch({
-      
-      # Selection et collecte des donnees d'interet avec dplyr
-      data <- tbl_duckdb %>% 
-        select(all_of(c(comb$id, comb$col))) %>% 
-        collect()
-      
-      names(data) <- c(id[1], col[1])
-      
-      print(paste("combinaison :",
-                  "id =", comb$id,
-                  "/ col =", comb$col))
-      break
-    }, error = function(e) {
-    })
-  }
+  # Selection et collecte des donnees d'interet avec dplyr
+  data <- tbl_duckdb %>% 
+    select(all_of(c(id, vars))) %>% 
+    collect()
   
   # Deconnexion de DuckDB
   dbDisconnect(con, shutdown = TRUE)
