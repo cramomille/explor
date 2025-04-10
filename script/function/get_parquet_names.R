4696372245f89a22199e5bd49efd9c8841aa0327
toinan.beroud@gmail.com <toinan.beroud@gmail.com>
2025-04-10 10:49:29 +0200

    nv fonction pour recuperer les noms des colonnes d'un fichier .parquet lorsque l'on ne les connait pas et qu'on souhaite utiliser la fonction open_parquet

---
+++ script/function/get_parquet_names.R
@@ -0,0 +1,55 @@
+#' @title Recuperation des noms des colonnes d'un fichier .parquet
+#' @description
+#' Cette fonction permet de recuperer les noms des colonnes presentes dans un
+#' fichier .parquet
+#' 
+#' @param dir le chemin vers le fichier .parquet ou le dossier qui contient les 
+#' chunks .parquet
+#' 
+#' @return 
+#' La fonction creer un vecteur
+#' 
+#' @examples
+#' names <- get_parquet_names(dir = "test/parquet/export/data1/")
+
+library(arrow)
+library(duckdb)
+library(dplyr)
+
+get_parquet_names <- function(dir) {
+  
+  # Creation de la connexion a DuckDB pour executer des requetes SQL
+  con <- dbConnect(duckdb())
+  
+  # Verification pour savoir si l'element est un fichier individuel ou un dossier
+  if (grepl("\\.parquet$", dir)) {
+    # Fichier individuel
+    parquet <- file.path(dir)
+    
+  } else {
+    # Dossier avec les chunks d'un fichier .parquet
+    parquet <- list.files(dir, pattern = "\\.parquet$", full.names = TRUE)
+    
+    if (length(parquet) == 0) {
+      stop(paste("Aucun fichier .parquet dans le dossier:", folder_dir))
+    }
+  }
+  
+  # Creation d'une vue SQL pour agreger les fichiers .parquet
+  query <- paste0(
+    "CREATE OR REPLACE VIEW all_data AS ",
+    paste0("SELECT * FROM read_parquet('", parquet, "')", collapse = " UNION ALL ")
+  )
+  dbExecute(con, query)
+  
+  # Chargement de la vue comme table DuckDB
+  tbl_duckdb <- tbl(con, "all_data")
+  
+  # Selection et collecte des noms des colonnes du fichier .parquet
+  names <- colnames(tbl_duckdb)
+  
+  # Deconnexion de DuckDB
+  dbDisconnect(con, shutdown = TRUE)
+  
+  return(names)
+}
