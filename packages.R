a1b229af77f716930e71487135e9f0366fa63f46
toinan.beroud@gmail.com <toinan.beroud@gmail.com>
2025-04-15 10:40:43 +0200

    script avec les packages pour les nv version de R

---
+++ packages.R
@@ -0,0 +1,8 @@
+
+install.packages("sf")
+install.packages("mapsf")
+install.packages("readxl")
+install.packages("rmapshaper")
+install.packages("ggplot2")
+
+install.packages("mapinsetr", repos = "https://riatelab.r-universe.dev")
