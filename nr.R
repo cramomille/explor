8f69088185d6f0088255175a35b1f97e952200ac
toinan.beroud@gmail.com <toinan.beroud@gmail.com>
2025-04-25 10:41:38 +0200

    installation package asf

--- nr.R
+++ nr.R
@@ -1,9 +1,25 @@
 
+#                                EXPLORATIONS ET TESTS SUR DES DONNEES A L'IRIS
+#
+#                                                                antoine beroud
+#                                                             nicolas raimbault
+#                                                                    avril 2025
+
+
+###############################################################################
+################################################### INSTALLATION DU PACKAGE ASF
+
+# Installation du package remotes pour permettre l'installation de packages sur GitLab
+install.packages("remotes")
+
+# Installation du package asf
+remotes::install_gitlab("atlas-social-de-la-france/asf", host = "gitlab.huma-num.fr")
+
+# Lancement des packages utilises dans le code
 library(sf)
 library(mapsf)
 library(asf)
 
-options(error = NULL)
 
 # install.packages("devtools")
 # devtools::install_github("alietteroux/subwork")
@@ -12,10 +28,12 @@ library(subwork)
 
 # Import des donnees "FT810" dans un data.frame nomme "FT810.data"
 FT810.data <- import(code = "FT810", type = "data")
+FT711.data <- import(code = "FT711", type = "data")
 
 cols <- "F_R_CP1"
+data <- FT810.data
 
-data <- FT810.data[, c(1:3, grep(cols, names(FT810.data)))]
+data <- data[, c(1:3, grep(cols, names(data)))]
 
 
 ###############################################################################
@@ -36,6 +54,9 @@ id_i <- unique(irisr$IRIS_CODE)
 id_if <- unique(irisr$IRISF_CODE)
 id_ir <- unique(irisr$IRISrS_CODE)
 
+# Agregation simple
+test <- rowsum(irisr[, c(7:24)], group = irisr$IRISrS_CODE)
+
 
 # Code des iris regroupes pour une analyse synchronique
 tabl <- mar$ar02$d.irisr.pass
@@ -50,6 +71,9 @@ irisr <- asf_data(tabl,
 
 sum(irisr$RP18_F_R_CP1_C1.Agri)
 sum(irisr$RP18_F_R_CP1_C1.Agri)
+
+
+
 
 
 ###############################################################################
