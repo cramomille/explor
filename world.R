a0ac9c51e9137ad52abb86296c29496d4fa11d3e
toinan.beroud@gmail.com <toinan.beroud@gmail.com>
2025-07-15 09:42:23 +0200

    maj min

--- world.R
+++ world.R
@@ -1,21 +1,98 @@
 
+#                                   BIDOUILLAGES ET MODELISATION DES MECANISMES 
+#                                               DU CAPITALISME SUR LES SOCIETES
+#                                                                              
+#                                                            cramomille petibon
+#                                                                  juillet 2025
 
 
-init_world <- function(n = 1000, seed = 1312) {
+###############################################################################
+init_world <- function(n = 1000, seed = NULL) {
   
-  set.seed(seed)
+  if (!is.null(seed)) set.seed(seed)
   
   digits <- nchar(as.character(n)) + 1
   
   data.frame(
-    i = sprintf(paste("%0", digits, "d"), 1:n),
-    a = sample(0:90, n, replace = TRUE, prob = dnorm(0:90, mean = 45, sd = 15)),
-    g = sample(0:100, n, replace = TRUE),
-    r = sample(0:100, n, replace = TRUE)
+    id = sprintf(paste("%0", digits, "d"), 1:n),
+    ag = sample(0:90, n, replace = TRUE, prob = dnorm(0:90, mean = 45, sd = 15)),
+    ge = sample(0:100, n, replace = TRUE),
+    ra = sample(0:100, n, replace = TRUE)
   )
 }
 
-world <- init_world() 
+
+###############################################################################
+nationalism <- function(world, seed = NULL) {
+  
+  if (!is.null(seed)) set.seed(seed)
+  
+  nb_countries <- sample(5:10, 1)
+  
+  centers_list <- lapply(1:nb_countries, function(i) {
+    sample(0:100, sample(1:3, 1), replace = FALSE)
+  })
+  
+  distance_matrix <- sapply(centers_list, function(centers) {
+    rowMeans(sapply(centers, function(c) dnorm(world$r, mean = c, sd = 10)))
+  })
+  
+  probas <- distance_matrix / rowSums(distance_matrix)
+  
+  country_id <- apply(probas, 1, function(p) sample(1:nb_countries, 1, prob = p))
+  
+  world$country <- country_id
+  
+  return(world)
+}
+
+
+###############################################################################
+colonialism <- function(world) {
+  
+  
+}
+
+
+
+
+
+world <- init_world(n = 1000) 
+world <- nationalism(world)
+
+
+graph(world, "ra", "country")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 capitalism <- function(world, years = 1) {
