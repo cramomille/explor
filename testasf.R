d4aa2aa25922a445e4d2cbb66f3f64faed29e43e
toinan.beroud@gmail.com <toinan.beroud@gmail.com>
2025-09-02 15:14:59 +0200

    maj min

--- testasf.R
+++ testasf.R
@@ -7,7 +7,7 @@
 # remove.packages("rmapshaper")
 # remove.packages("mapinsetr")
 # remove.packages("asf")
-
+# 
 # remotes::install_gitlab("atlas-social-de-la-france/asf",
 #                         host = "gitlab.huma-num.fr",
 #                         build_vignettes = TRUE)
@@ -18,57 +18,60 @@ library(asf)
 
 
 # Fond de carte ---------------------------------------------------------------
-mar <- asf_mar(maille = "irisrs")
+mar <- asf_mar(maille = "comr")
 
-irisf <- mar$geom
 tabl <- mar$tabl
+geom <- mar$geom
 
 # Repositionnement des DROM 
-irisf <- asf_drom(irisf, 
-                  id = "IRISF_CODE")
+geom <- asf_drom(geom, 
+                 id = "IRISF_CODE")
 
-# Creation du fond des irisr a partir des irisf
-irisr <- asf_fond(irisf,   
-                  tabl, 
-                  by = "IRISF_CODE", 
-                  maille = "IRISrS_CODE") 
+# Creation du fond des comr a partir des comf
+comr <- asf_fond(geom, 
+                 tabl, 
+                 by = "COMF_CODE", 
+                 maille = "COMR_CODE") 
 
 # Creation de zooms
-z <- asf_zoom(irisr, 
-              places = c("Paris", "Avignon", "Bergerac", "Annecy"))
+z <- asf_zoom(comr, 
+              places = c("Paris", 
+                         "Avignon", 
+                         "Bergerac", 
+                         "Annecy"))
 
 zoom <- z$zooms
 label <- z$labels
 point <- z$points
 
 # Simplification des geometries du fond de carte
-irisr_simply <- asf_simplify(irisr)
+comr_simply <- asf_simplify(comr)
 
 
-# Data ------------------------------------------------------------------------
-iris_data <- read.csv("input/csp_2020.csv")
+# Tableau de donnees ----------------------------------------------------------
+data <- read.csv("input/csp_2020.csv")
 
-irisr_data <- asf_data(iris_data, 
-                       tabl, 
-                       by.x = "IRIS",
-                       by.y = "IRIS_CODE", 
-                       maille = "IRISrS_CODE",
-                       vars = c(4:13), 
-                       funs = c("sum"))
+comr_data <- asf_data(data, 
+                      tabl, 
+                      by.x = "COM", 
+                      by.y = "COM_CODE", 
+                      maille = "COMR_CODE", 
+                      vars = c(4:13), 
+                      funs = c("sum"))
 
 
 # Jointure --------------------------------------------------------------------
-fondata <- asf_fondata(irisr_simply,
-                       zoom,
-                       irisr_data,
-                       by = "IRISrS_CODE")
+fondata <- asf_fondata(comr_simply, 
+                       zoom, 
+                       comr_data, 
+                       by = "COMR_CODE")
 
 
 # Creation de cartes ----------------------------------------------------------
 # Creation des limites departementales
-irisf$DEP_CODE <- substr(irisf$IRISF_CODE, 1, 2)
+geom$DEP_CODE <- substr(geom$COMF_CODE, 1, 2)
 
-dep <- asf_borders(irisf, by = "DEP_CODE", keep = 0.1)
+dep <- asf_borders(geom, by = "DEP_CODE", keep = 0.01)
 
 # Choix d'une palette de couleurs
 pal <- asf_palette("seq")
@@ -78,7 +81,7 @@ mf_map(fondata,
        var = "C20_POP15P_CS6", 
        type = "choro", 
        nbreaks = 6, 
-       pal = pal,
+       pal = pal, 
        border = NA)
 
 mf_map(dep, 
@@ -87,7 +90,7 @@ mf_map(dep,
        add = TRUE)
 
 mf_map(point, 
-       col = "red",
+       col = "red", 
        add = TRUE)
 
 mf_label(label, 
@@ -127,5 +130,3 @@ asf_plot_vars(tmp,
               typo = "TAAV2017",
               pal = pal
               )
-
-
